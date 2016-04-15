3aadaf77def7ad94f9d2282d132359869d72a895
captucaptu <captucaptu@gmail.com>
2016-04-15 23:24:30 +0800

    fix text

--- js/twice.js
+++ js/twice.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var key = '1iD6RcrJf2eV9pbE301MgYZFAwjKU2sbVQ32yTWULJf8';
 	var userURL = 'https://spreadsheets.google.com/feeds/list/' + key + '/1/public/values?alt=json';
 	
-	// 總數
+	//總數
 	// queryTotal();
 	// function queryTotal(){
 	// 	var total = 0;
@@ -15,12 +15,12 @@ $(document).ready(function(){
 
 	// 			// 呈現表格 rows 資料
 	// 			var trows = json.feed.entry;
-	// 			//var num = 0;
+				
 	// 			$.each(trows, function (i, data) {
-	// 					total += Number(data.gsx$訂購數量.$t);
-					
+	// 				total += Number(data.gsx$購買數量.$t);
 	// 			});
-	// 			//total = (red+white);
+	// 			//total = (num+num2);
+	// 			console.log(total);
 	// 			$('.all span').html(total);
 	// 		},
 	// 		//錯誤判斷
@@ -72,7 +72,7 @@ $(document).ready(function(){
 						$('li.num1').text("PINK 數量："+booker[7]);
 						$('li.num2').text("MINT 數量："+booker[8]);
 						$('li.ac').text("匯款單資訊："+booker[6]);
-						$('li.card').text("是否拆卡"+booker[9]);
+						$('li.card').text("是否拆卡："+booker[9]);
 						$('li.cardsort').text("需換專卡排序："+booker[10]);
 						$('li.card2').text("音源卡是否提供："+booker[11]);
 						$('li.poster').text("是否需要海報筒："+booker[12]);
